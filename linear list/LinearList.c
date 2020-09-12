26a301aec5eb5a54039add1371fa2ca48564bcfb
chaosprogrammer <1499015923@qq.com>
2020-09-12 22:21:18 +0800

    build

---
+++ linear list/LinearList.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int Elemtype;
+#define MaxSize 50 //线性表最大长度
+typedef struct LinearList 
+{
+   ElemType data{MaxSize};//顺序表元素
+   int length;
+
+}SqList;
+
+void main()
+{
+}
